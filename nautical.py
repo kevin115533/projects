6ce23a37ba155b7cd9a1e02eb4dde62ed2d00e85
Kevin Tran <kevin115533@yahoo.com>
2019-09-20 11:01:13 -0600

    Update nautical.py

--- nautical.py
+++ nautical.py
@@ -4,6 +4,17 @@ Author: Kevin Tran
 
 The purpose of this program is to convert kilometers to nautical miles.
 
-1. Get user input for the length of single edge of a cube.
+1. Get user input kilometers to convert.
+
+2. Calculate nautical miles using provided kilometer using formula
+    nm = (km/10000) * 5400
+
+3. Print results    
 
 """
+
+km = int(input("Enter the amount of kilometers to convert: "))
+
+nm = (km/10000) * 5400
+
+print("The conversion of " + str(km) +" kilometers is approximatley " + str(round(nm)) + " nautical miles")
